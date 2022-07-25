58a6132d02098b1cc7b0950c0adedd065bfc5d74
Noah Thovson <noahthovson@gmail.com>
2022-07-24 20:05:06 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from random import randint
+
+steamPath = "C:/Users/zanni/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Steam"
+
+def getListOfGames(path):
+    print(f"Getting games from {path}...")
+    dirList = os.listdir(path)
+    print("Found:")
+    print(dirList)
+
+    return dirList
+
+def pickRandomFromList(choices):
+    print("Picking...")
+    choice = choices[randint(0, len(choices))]
+    print("Chose '{choices}'")
+
+    return choice
+
+def launch(choice):
+    print(f"Launching {choice}")
+    os.startfile(f"{steamPath}/{choice}")
+
+    return
+
+def main():
+    # Get list of steam games
+    games = getListOfGames(steamPath)
+
+    # Pick a random one
+    choice = pickRandomFromList(games)
+
+    # Launch
+    launch(choice)
+
+    return
+
+main()
